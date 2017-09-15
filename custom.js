9b0b83427f6ae0ae7fcb6aec5ebf11752fb1ac6f
Ashish Rai <ashishrai321@gmail.com>
2017-09-15 18:21:37 +0530

    Add files via upload

--- custom.js
+++ custom.js
@@ -251,5 +251,65 @@ $(document).ready(function() {
 })
 
 
+(function($){
+	$.fn.viewportChecker = function(useroptions){
+	// Define options and extend with user
+	var options = {
+			classToAdd: 'visible',
+			offset: 100,
+			currIndex: 0,
+			callbackFunction: function(elem){}
+		};
+	$.extend(options, useroptions);
+	
+	// Cache the given element and height of the browser
+	var $elem = this,
+		windowHeight = $(window).height();
+	
+		this.checkElements = function(){
+			// Set some vars to check with
+			var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html'),
+				viewportTop = $(scrollElem).scrollTop(),
+				viewportBottom = (viewportTop + windowHeight);
+		
+			$elem.each(function(index){
+				
+				var $obj = $(this);
+				// If class already exists; quit
+				if ($obj.hasClass(options.classToAdd)){
+					
+					return;
+				}
+				
+				// define the top position of the element and include the offset which makes is appear earlier or later
+				var elemTop = Math.round( $obj.offset().top ) + options.offset,
+					elemBottom = elemTop + ($obj.height());
+		
+				// Add class if in viewport
+				if ((elemTop < viewportBottom) && (elemBottom > viewportTop)){
+					$obj.addClass(options.classToAdd);
+					currIndex = index;
+					// Do the callback function. Callback wil send the jQuery object as parameter
+					options.callbackFunction($obj);
+				}
+			});
+		};
 
+	// Run checkelements on load and scroll
+		$(window).scroll(this.checkElements);
+		this.checkElements();
+	
+	// On resize change the height var
+		$(window).resize(function(e){
+			windowHeight = e.currentTarget.innerHeight;
+		});
+	};
+})(jQuery);
+
+if($('section.bannerBlock').length){
+	$('section').viewportChecker({
+		//classToAdd: 'animated fadeInBottom', // Class to add to the elements when they are visible
+		//offset: 10    
+	}); 
+}
 
